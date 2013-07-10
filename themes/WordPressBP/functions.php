bfca61916d1d7cdad39e96bc8721cd86fb3b6bc8
Andrej Čremožnik <andrej@cremoznik.si>
2013-07-10 17:10:39 +0200

    Refactoring theme functions

--- themes/WordPressBP/functions.php
+++ themes/WordPressBP/functions.php
@@ -1,80 +1,92 @@
 <?php
 
 /*
- ********* Load Text Domain http://codex.wordpress.org/I18n_for_WordPress_Developers *********
+ * Set up theme's defaults, register various features...
  */
-function theme_textdomain() {
+function theme_setup() {
+
+	// Load theme text domain
 	load_theme_textdomain('WordPressBP', get_template_directory() . '/lang');
-}
-add_action('after_setup_theme', 'theme_textdomain');
 
+	// Custom editor style
+	//add_editor_style('editor-style.css');
 
-/*
- ********* Menus (register_nav_menus) *********
- */
-function create_menus() {
+	// Register navigation menus
 	register_nav_menus(array(
 		'primary' => __('Main menu', 'WordPressBP')
 	));
-}
-add_action('init', 'create_menus');
 
+	// Enable support for certain features
+	add_theme_support('automatic-feed-links');
+	//add_theme_support('post-formats');
+	//add_theme_support('custom-background');
+	//add_theme_support('custom-header');
+
+	// TODO: better comments
+	add_theme_support('post-thumbnails');
+	add_image_size('size_name', 300, 200, true);
+	// To list the sizes in WP media manager dropdown, add them to
+	// theme_image_sizes function below
+	add_filter('image_size_names_choose', 'theme_image_sizes' );
+
+}
+
+function theme_image_sizes($sizes) {
+	$sizes['size_name'] = __('New size', 'WordPressBP');
+	return $sizes;
+}
+
+add_action('after_setup_theme', 'theme_setup');
 
 /*
- ********* Sidebars (register_sidebar) *********
+ * Set up theme's sidebars
  */
-function create_sidebars() {
+function theme_widgets_init() {
 	register_sidebar(array(
 		'name' => __('Sidebar 1', 'WordPressBP'),
 		'id'   => 'sidebar1'
 	));
 }
-add_action('init', 'create_sidebars');
+add_action('widgets_init', 'theme_widgets_init');
+
+
 
 
 /*
- ********* Styles (wp_register_style, wp_enqueue_style) *********
+ * Get file timestamp for automatic cache busting
+ * - http://calendar.perfplanet.com/2012/using-nginx-php-fpmapc-and-varnish-to-make-wordpress-websites-fly/
  */
-function theme_styles() {
+function autoVer($url){
+	$name = explode('.', $url);
+	$lastext = array_pop($name);
+	array_push(
+		$name,
+		filemtime($_SERVER['DOCUMENT_ROOT'] . parse_url($url, PHP_URL_PATH)),
+		$lastext);
+	echo implode('.', $name) ;
+}
+
+/*
+ * Register styles and scripts for frontend
+ *
+ * - Styles (wp_register_style, wp_enqueue_style)
+ * - Scripts (wp_register_script, wp_enqueue_script)
+ */
+function theme_scripts_styles() {
 	// Register styles
-	wp_register_style('default', get_template_directory_uri().'/style.css', false, null, 'all');
+	wp_register_style('default', get_template_directory_uri() . autoVer('/style.css'), false, null, 'all');
+
+	// Register scripts
+	wp_register_script('global', get_template_directory_uri() . autoVer('/js/global.js'), array('jquery'), null, true);
 
 	// Enqueue styles
 	wp_enqueue_style('default');
-}
-add_action('wp_enqueue_scripts', 'theme_styles');
-
-// Custom editor style
-//add_editor_style('editor_style.css');
-
-
-/*
- ********* Scripts (wp_register_script, wp_enqueue_script) *********
- */
-function theme_scripts() {
-	// Register scripts
-	wp_register_script('global', get_template_directory_uri().'/js/global.js', array('jquery'), '1.0', true);
 
 	// Enqueue scripts
 	wp_enqueue_script('jquery');
 	wp_enqueue_script('global');
 }
-add_action('wp_enqueue_scripts', 'theme_scripts');
-
-
-/*
- ********* Theme support (add_theme_support) *********
- */
-add_theme_support('automatic-feed-links');
-//add_theme_support('post-formats');
-add_theme_support('post-thumbnails');
-//add_theme_support('custom-background');
-//add_theme_support('custom-header');
-
-/*
- ********* Image sizes (add_image_size) *********
- */
-//add_image_size('banner', 960, 250, true);
+add_action('wp_enqueue_scripts', 'theme_scripts_styles');
 
 
 /*
