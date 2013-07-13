2fdf29c150dc4146f39e7fa6b8491b71635a2bca
Andrej Čremožnik <andrej@cremoznik.si>
2013-07-13 20:12:15 +0200

    Bash setup script

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-	echo -e "Usage:\n $0 <namespace> <project/path>\n"
+	echo -e "Usage:\n $0 <namespace> <project_path>\n"
 	echo " <namespace>:    Alphanumeric name for your project. Used for namespacing functions, textdomains etc."
 	echo " <project_path>: Path to where the plugins and themes directories will be set up."
 	exit
 fi
 
 namespace="$1"
-project_path="$2"
+project_path=${2%/}
 
 if [ ! -d $project_path ]; then
 	echo "The directory $project_path does not exist. Exiting..."
@@ -17,8 +17,23 @@ fi
 
 echo "Creating a working copy of WordPressBP in $project_path"
 
-git archive --format=tar master | tar -x -C $project_path
+git archive --format=tar devel | tar -x -C $project_path
 
-echo "Done."
+echo "Renaming folders and files..."
 
-echo "TODO: namespacing"
+for f in `find $project_path -depth -name '*WordPressBP*'`; do
+	# path before directory/file name
+	tmp_path=${f%/*}
+	# directory/file name
+	tmp_file=$(basename $f)
+	# renamed directory/file
+	tmp_file_new=${tmp_file/WordPressBP/$namespace}
+	# do the move
+	mv ${tmp_path}/${tmp_file} ${tmp_path}/${tmp_file_new}
+done
+
+echo "Namespacing file contents..."
+
+find $project_path -type f -print0 | xargs -0 sed -i "s/WordPressBP/${namespace}/g"
+
+echo "All done. Happy hacking!"
