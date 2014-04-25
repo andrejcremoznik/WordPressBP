5ce56ecc133af7d5bf1d0c3fb7088ab3cb683c74
Andrej Čremožnik <andrej@cremoznik.si>
2014-04-25 14:25:50 +0200

    Add page template

---
+++ themes/WordPressBP/page.php
@@ -0,0 +1,14 @@
+<?php get_header() ?>
+
+<main class="main" role="main">
+	<?php if(have_posts()): while(have_posts()): the_post() ?>
+		<article <?php post_class() ?>>
+			<h1><?php the_title() ?></h1>
+			<?php the_content() ?>
+		</article>
+	<?php endwhile; endif ?>
+</main>
+
+<?php get_sidebar() ?>
+
+<?php get_footer() ?>
