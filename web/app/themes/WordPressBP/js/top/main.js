1a9557b45bde4bd255f0be58a690220bf99b180b
Andrej Čremožnik <andrej@cremoznik.si>
2014-11-28 15:39:33 +0100

    Add modern IE version detection

--- web/app/themes/WordPressBP/js/top/main.js
+++ web/app/themes/WordPressBP/js/top/main.js
@@ -1,30 +1,27 @@
-(function() {
+(function(w, d, className) {
   'use strict';
 
   var
-    w = window,
-    d = document,
     e = d.documentElement,
-    g = d.getElementsByTagName('body')[0],
-    htmlClass = 'js' // Replace .no-js with .js
+    g = d.getElementsByTagName('body')[0]
   ;
 
   // Is touch supported?
   w.isTouch = ('ontouchstart' in w);
 
-  // Check if browser is IE and concat ie-<version> with htmlClass
-  if(/MSIE (\d+\.\d+);/.test(navigator.userAgent)) {
-    var ieVerion = new Number(RegExp.$1);
-    htmlClass = htmlClass + ' ie' + ieVerion;
+  // Check if browser is IE and concat ie-<version> with className
+  if(/MSIE (\d+\.\d+);/.test(navigator.userAgent) || (Object.hasOwnProperty.call(w, 'ActiveXObject') && !w.ActiveXObject)) {
+    var ieVersion = new Number(RegExp.$1) <= 10 ? new Number(RegExp.$1) : Object.hasOwnProperty.call(w, 'ActiveXObject') && !w.ActiveXObject ? '11' : '';
+    className = className + ' ie' + ieVersion;
   }
   // Add touchscreen class if touch supported, desktop otherwise
-  htmlClass = htmlClass + ((isTouch) ? ' touchscreen' : ' desktop');
+  className = className + ((isTouch) ? ' touchscreen' : ' desktop');
 
-  // Replace class on <html> with htmlClass
-  e.className = htmlClass;
+  // Replace class on <html> with className
+  e.className = className;
 
   // Save viewport sizes
   w.viewportWidth  = w.innerWidth  || e.clientWidth  || g.clientWidth;
   w.viewportHeight = w.innerHeight || e.clientHeight || g.clientHeight;
 
-}());
+}(window, document, 'js'));
