1deaf5e4d2fd2a10937c35d1df565df96e5efd46
Andrej Čremožnik <andrej@cremoznik.si>
2015-05-04 14:57:13 +0200

    Small tweaks

--- web/app/themes/WordPressBP/header.php
+++ web/app/themes/WordPressBP/header.php
@@ -2,7 +2,7 @@
 <html class="no-js" <?php language_attributes() ?>>
 <head>
 <meta charset="<?php bloginfo('charset') ?>">
-<meta name="viewport" content="width=device-width,initial-scale=1">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 <?php wp_head() ?>
 </head>
 <body <?php body_class() ?>>
