8c2c4221bdf561adafd0979fbdfd0278fd875f42
Andrej Čremožnik <andrej@cremoznik.si>
2015-07-23 16:44:18 +0200

    Small WPBP theme refactor, add more CSS helper classes and JS boilerplate

--- web/app/themes/WordPressBP/page.php
+++ web/app/themes/WordPressBP/page.php
@@ -1,11 +1,11 @@
 <?php get_header() ?>
 
 <main class="main" role="main">
-  <?php if(have_posts()): while(have_posts()): the_post() ?>
+  <?php if (have_posts()): while (have_posts()): the_post() ?>
     <article <?php post_class() ?>>
       <h1><?php the_title() ?></h1>
       <?php the_content() ?>
-      <?php if(comments_open() || get_comments_number()) comments_template() ?>
+      <?php if (comments_open() || get_comments_number()) comments_template() ?>
     </article>
   <?php endwhile; endif ?>
 </main>
