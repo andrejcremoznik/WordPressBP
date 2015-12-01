8f5c08be9082132556aaf672f8244e1c75cce032
Andrej Čremožnik <andrej@cremoznik.si>
2015-12-01 09:26:33 +0100

    Move Nginx conf inside config directory

--- setup.sh
+++ setup.sh
@@ -14,20 +14,20 @@ project_repo=${2%/}/repo
 
 # Make sure $project_path exists, is writeable and clean
 if [ ! -d $project_path ]; then
-  echo "The directory $project_path does not exist. Creating..."
+  echo "The directory $project_path does not exist. Creating…"
   mkdir ${project_path}
   if [ $? -ne 0 ] ; then
-    echo "Cannot create $project_path. Check write permissions. Exiting..."
+    echo "Cannot create $project_path. Check write permissions. Exiting…"
   fi
 else
-  echo "The directory $project_path exist. Emptying..."
+  echo "The directory $project_path exist. Emptying…"
   if [ -w $project_path ] ; then
     cd $project_path
     rm -f .[^.] .??*
     rm -fr *
     cd - 2>&1 >/dev/null
   else
-    echo "Cannot empty $project_path. Check write permissions. Exiting..."
+    echo "Cannot empty $project_path. Check write permissions. Exiting…"
   fi
 fi
 
@@ -40,7 +40,7 @@ echo "Creating a working copy of WordPressBP in $project_repo"
 git archive --format=tar ${3:-master} | tar -x -C $project_repo
 
 # Replace WordPressBP in folder and file names with $namespace
-echo "Renaming folders and files..."
+echo "Renaming folders and files…"
 
 for f in `find $project_repo -depth -name '*WordPressBP*'`; do
   # path before directory/file name
@@ -57,17 +57,17 @@ echo "# ${namespace}" > ${project_repo}/README.md
 cp ${project_repo}/.env.example ${project_repo}/.env
 
 # Replace WordPressBP in file contents with $namespace
-echo "Namespacing file contents..."
+echo "Namespacing file contents…"
 
 find ${project_repo}/ -type f -print0 | xargs -0 sed -i "s/WordPressBP/${namespace}/g"
 
 # Set up Nginx vhost
-echo "Setting up Nginx configuration in $project_path/etc"
+echo -e "Setting up Nginx configuration in $project_path/etc\n"
 
-sed -i "s|{project_path}|${project_path}|g" ${project_repo}/etc/nginx.conf
-cp ${project_repo}/etc/nginx.conf ${project_path}/etc/
+sed -i "s|{project_path}|${project_path}|g" ${project_repo}/config/nginx/nginx.conf
+mv ${project_repo}/config/nginx/nginx.conf ${project_path}/etc/
 
-echo -e "\nDone. Next we will set up WordPress. To continue you will need:"
+echo "Done. Next we will set up WordPress. To continue you will need:"
 echo "1. MySQL user with CREATE DATABSE privileges OR a user with basic use privileges for an existing database"
 echo "2. \"npm\", \"bower\", \"grunt\", \"composer\" and \"wp\" (WP-CLI) available in your PATH"
 echo -e "If you are not sure about any of these, please read https://gist.github.com/andrejcremoznik/07429341fff4f318c5dd\n"
@@ -75,13 +75,13 @@ echo -e "If you are not sure about any of these, please read https://gist.github
 read -e -p "Do you wish to continue setting up WordPress? (y/n): " cont
 if [ $cont != "y" ]; then exit; fi
 
-echo "Checking for required software..."
+echo "Checking for required software…"
 
-command -v composer >/dev/null 2>&1 || { echo >&2 "Composer not installed. Aborting..."; exit 1; }
-command -v npm >/dev/null 2>&1 || { echo >&2 "NPM not installed. Aborting..."; exit 1; }
-command -v bower >/dev/null 2>&1 || { echo >&2 "Bower not installed. Aborting..."; exit 1; }
-command -v grunt >/dev/null 2>&1 || { echo >&2 "Grunt not installed. Aborting..."; exit 1; }
-command -v wp >/dev/null 2>&1 || { echo >&2 "WP-CLI not installed. Aborting..."; exit 1; }
+command -v composer >/dev/null 2>&1 || { echo >&2 "Composer not installed. Aborting…"; exit 1; }
+command -v npm >/dev/null 2>&1 || { echo >&2 "NPM not installed. Aborting…"; exit 1; }
+command -v bower >/dev/null 2>&1 || { echo >&2 "Bower not installed. Aborting…"; exit 1; }
+command -v grunt >/dev/null 2>&1 || { echo >&2 "Grunt not installed. Aborting…"; exit 1; }
+command -v wp >/dev/null 2>&1 || { echo >&2 "WP-CLI not installed. Aborting…"; exit 1; }
 
 echo "All there."
 
@@ -102,23 +102,23 @@ sed -i "s/localhost/${dbhost}/g" .env
 read -e -p "Database table prefix: " -i "wpdb_" dbprefix
 sed -i "s/db_prefix/${dbprefix}/g" .env
 
-echo -e "Running composer install...\n"
+echo -e "Running composer install…\n"
 composer install
 
-echo -e "Running NPM install...\n"
+echo -e "Running NPM install…\n"
 npm install
 
-echo -e "Running Bower install...\n"
+echo -e "Running Bower install…\n"
 bower install
 
-echo -e "Running Grunt...\n"
+echo -e "Running Grunt…\n"
 grunt
 
-read -e -p "Does user $dbuser have CREATE DATABASE privileges? Create it now? (y/n): " dbperms
+read -e -p "Does user $dbuser have CREATE DATABASE privileges? Create database now? (y/n): " dbperms
 if [ $dbperms == "y" ]; then
   wp db create
 else
-  read -p "Please create $dbname database manually and grant $dbuser all basic use privileges. Press [Enter] when done..."
+  read -p "Please create $dbname database manually and grant $dbuser all basic use privileges. Press [Enter] when done…"
 fi
 
 wp db reset --yes
@@ -142,7 +142,6 @@ echo "Creating developer admin account (login: dev / dev)"
 wp user create dev dev@dev.dev --user_pass=dev --role=administrator
 
 cd - 2>&1 >/dev/null
-
 
 echo -e "All done.\n"
 echo "Check $project_path/etc/nginx.conf, make sure the file is included in /etc/nginx/nginx.conf and reload the server."
