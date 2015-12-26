c616b56eb752888b378787c1b09abdbbabd521ac
Andrej Čremožnik <andrej@cremoznik.si>
2015-12-26 22:36:27 +0100

    Deprecate Grunt: remove grunt configs

--- config/grunt/csso.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  build: {
-    expand:  true,
-    flatten: true,
-    src:     'web/app/themes/WordPressBP/assets/*.css',
-    dest:    'web/app/themes/WordPressBP/assets/'
-  }
-}
