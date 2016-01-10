80d5ee527536dac97c29085b95aba0c6cc206ce9
Andrej Čremožnik <andrej@cremoznik.si>
2016-01-10 13:43:00 +0100

    Add agent to SSH config, add some file extensions to .gitignore

--- config/deploy/config.js
+++ config/deploy/config.js
@@ -3,12 +3,14 @@ module.exports = {
     staging: {
       host: 'hostname',
       port: 22,
-      username: 'user'
+      username: 'user',
+      agent: process.env.SSH_AUTH_SOCK
     },
     production: {
       host: 'hostname',
       port: 22,
-      username: 'user'
+      username: 'user',
+      agent: process.env.SSH_AUTH_SOCK
     }
   },
   deployEnvPaths: {
