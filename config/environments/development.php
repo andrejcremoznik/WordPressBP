741fba29be5609e083dea8678e8c59fce9e37646
Andrej Cremoznik <andrej@cremoznik.si>
2018-03-13 19:47:24 +0100

    Add a dump function for development

--- config/environments/development.php
+++ config/environments/development.php
@@ -3,3 +3,8 @@
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
+
+function dump($var) {
+  echo '<pre style="font:12px/1 monospace;">';
+  die(var_dump($var));
+}
